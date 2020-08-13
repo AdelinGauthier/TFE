af2f7e8dea69e14947946031deee5bc0b0c63c93
AdelinGauthier <63398248+AdelinGauthier@users.noreply.github.com>
2020-08-14 00:28:17 +0200

    update settings.py to deploy

--- noraMazy/settings.py
+++ noraMazy/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['noramazy.herokuapp.com']
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
