776505b006b4bf9e061ea572133aa14adba5d980
AdelinGauthier <63398248+AdelinGauthier@users.noreply.github.com>
2020-08-14 01:37:20 +0200

    test deploy

--- noraMazy/settings.py
+++ noraMazy/settings.py
@@ -46,7 +46,7 @@ STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Accueil/media').replace('\\', '/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Accueil/media')
 
 
 
