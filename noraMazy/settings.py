fd7fbdc2089a626b4866adf54587d4bdc19e8219
AdelinGauthier <63398248+AdelinGauthier@users.noreply.github.com>
2020-08-14 00:56:08 +0200

    staticFile Dir broken

--- noraMazy/settings.py
+++ noraMazy/settings.py
@@ -37,9 +37,9 @@ STATIC_TMP = os.path.join(BASE_DIR, 'static')
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
