e234533ce35a837ed5f5d7184152132e779e47f1
AdelinGauthier <63398248+AdelinGauthier@users.noreply.github.com>
2020-08-14 04:17:45 +0200

    superuser4

--- users/models.py
+++ users/models.py
@@ -61,7 +61,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'forename', 'adress1', 'adress2', 'dateNaiss', 'phone']
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
