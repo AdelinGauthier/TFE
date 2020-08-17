4b33c43192606c7bcf85b704a655d34673dde52e
AdelinGauthier <63398248+AdelinGauthier@users.noreply.github.com>
2020-08-17 17:39:47 +0200

    smtp port

--- noraMazy/settings.py
+++ noraMazy/settings.py
@@ -170,7 +170,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "nora.mazy.contact@gmail.com"
 EMAIL_HOST_PASSWORD = 'NoMaz/33'
-EMAIL_PORT = '465'
+EMAIL_PORT = '587'
 
 
 
