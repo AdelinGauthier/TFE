9f7cc85f6dce975c3aa8e2e380eec44b2ebf2390
AdelinGauthier <63398248+AdelinGauthier@users.noreply.github.com>
2021-01-21 21:18:14 +0100

    Mail finalisation

--- Accueil/views.py
+++ Accueil/views.py
@@ -19,13 +19,18 @@ def contact(request):
             form.save()
             mail = ''
             msg = 'Salut Nora, je te contact via le formulaire de demande : \n'
+            conf = 'Je vous confirme par la présente que votre message a bien été envoyé. Le voici : \n\n'
             for item in form:
                 if item.name == 'email':
                     mail = str(item.value())
                 msg += str(item.value()) + '\n'
+                conf += str(item.value()) + '\n'
             msg += 'Merci beaucoup ! J attend ta réponse !'
-            send_mail('Formulaire de contact', str(msg), request.user,
+            conf += '\nJe traite votre demande dans les plus brefs délais\n Cordialement, \n Nora Mazy'
+            send_mail('Formulaire de contact', str(msg), request.user.email,
                       ['nora.mazy.contact@gmail.com'], fail_silently=False)
+            send_mail('Confirmation de demande', str(conf), 'nora.mazy.contact@gmail.com',
+                      [request.user.email], fail_silently=False)
             return redirect('Accueil')
     else:
         form = ContactForm()
